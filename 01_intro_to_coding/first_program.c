f26068e1f484c8206dea1afae3248155e476875d
CatBrainJordanABaumann <bbaumann2194@floridapoly.edu>
2025-08-20 14:52:07 +0000

    8/20 stuff

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,21 @@
+/*
+
+08/20/25
+This is a multi-line comment.
+Jordan (Bruce) Baumann
+My first program in C... for this course.
+
+*/
+
+// This is an example single line comment
+// Yippee I love preprocessor directives! #include is not as cool as #define imo but still cool.
+//.h files are header files and are made to be included by other .h files or by .c files
+#include <stdio.h> // Standard input output
+
+// Create the main function
+int main() // <- Void can go in the parenthesis but it's optional and doesn't fit my programming style so it has been left out for now.
+{
+    printf("Greetings universe!\n");
+
+    return 0;
+}
