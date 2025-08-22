074069d5fe0d319a675be61d1a8ce0b6f46101f7
CatBrainJordanABaumann <bbaumann2194@floridapoly.edu>
2025-08-22 14:39:38 +0000

    Day 3 things. Added more to first_program.c to follow lecture and added 5 new unix commands.

--- 01_intro_to_coding/first_program.c
+++ 01_intro_to_coding/first_program.c
@@ -3,19 +3,22 @@
 08/20/25
 This is a multi-line comment.
 Jordan (Bruce) Baumann
-My first program in C... for this course.
+My first program in C for this course.
 
 */
 
 // This is an example single line comment
-// Yippee I love preprocessor directives! #include is not as cool as #define imo but still cool.
 //.h files are header files and are made to be included by other .h files or by .c files
 #include <stdio.h> // Standard input output
 
 // Create the main function
-int main() // <- Void can go in the parenthesis but it's optional and doesn't fit my programming style so it has been left out for now.
-{
-    printf("Greetings universe!\n");
+// Same-line squiggly brackets is referred to as K&H style
+int main() { // <- void can go in the parenthesis but it's optional and doesn't fit my programming style so it has been left out for now.
+    // All the stuff in a block of squiggly brackets "{}" are referred to as a "block" of code
+    printf("Hello\nworld!\n\n");
 
-    return 0;
+    printf("I\nam\nready\nto\ncode!\n\n");
+
+    printf("Farewell\nworld!\n");
+    return 0; // <- Anything other than a 0 is an error
 }
